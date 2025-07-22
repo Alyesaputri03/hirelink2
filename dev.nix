59c6e4913f75524d0f34d8aa0e0e3463a4e3dd3a
akuichsann <akuichsann@gmail.com>
2025-07-22 17:36:58 +0000

    Add Nix flake config with Flutter

---
+++ dev.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flutter DevShell with Dart 3.7.2+";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.flutter
+            pkgs.git
+          ];
+        };
+      });
+}
